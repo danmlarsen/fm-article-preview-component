661efa36d16f8fc8e5c7ce12fe30ca14a2b2e6d1
Dan M. Larsen <larsendanmarius@gmail.com>
2024-08-03 15:10:39 +0200

    Minor adjustments

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ btn.addEventListener("click", function () {
   btn.classList.toggle("preview__share-btn--active");
   shareMenu.classList.toggle("hidden");
 });
+
+window.addEventListener("click", (e) => {
+  const clickedBtn = e.target.closest(".preview__share-btn");
+  const clickedMenu = e.target.closest(".share-menu");
+  if (
+    btn.classList.contains("preview__share-btn--active") &&
+    !clickedBtn &&
+    !clickedMenu
+  ) {
+    shareMenu.classList.add("hidden");
+    btn.classList.remove("preview__share-btn--active");
+  }
+});
