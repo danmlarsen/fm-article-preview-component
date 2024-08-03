9a9802a60df9a920d29fbb55c4a5c44f06561d05
Dan M. Larsen <larsendanmarius@gmail.com>
2024-08-03 13:31:05 +0200

    Mostly finished. Needs a bit of refactoring

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 const btn = document.querySelector(".preview__share-btn");
-const btnIcon = btn.querySelector(".preview__share-icon");
 const shareMenu = document.querySelector(".share-menu");
 
 btn.addEventListener("click", function () {
-  shareMenu.classList.toggle("hidden");
   btn.classList.toggle("preview__share-btn--active");
+  shareMenu.classList.toggle("hidden");
 });
